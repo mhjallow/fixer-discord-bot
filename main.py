6b69c1fbef64c05169eec615dd2d72efa672faea
Mohamed Jallow <43049419+mhjallow@users.noreply.github.com>
2025-05-21 15:20:28 -0400

    Add event handling for messages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+import re
 
 #Load .env file
 load_dotenv()
@@ -16,5 +17,11 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(F"✅ Logged in as {client.user}")
+
+@client.event
+async def on_message(message): #This function runs every time a message is sent anywhere the bot can see.
+    if message.author.bot:
+        return #Ignore messages sent by bots
+    print(f"Message from {message.author}: {message.content}")
 
 client.run(TOKEN)
